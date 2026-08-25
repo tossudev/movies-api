cf8f98874080951ba673948d6fdb43941389b8d2
Jere Meskanen <jere@macbook-2.local>
2026-08-25 13:41:58 +0300

    add: pagination helper, connected to actor.

--- internal/handlers/helpers.go
+++ internal/handlers/helpers.go
@@ -7,6 +7,11 @@ import (
 	"strconv"
 )
 
+type Pagination struct {
+	page   int
+	offset int
+}
+
 func decodeRequest[T any](r *http.Request) (req T, err error) {
 	return req, json.NewDecoder(r.Body).Decode(&req)
 }
@@ -18,4 +23,33 @@ func getID(r *http.Request) (int, error) {
 	}
 
 	return id, nil
+}
+
+func getPagination(r *http.Request) (page int, size int, err error) {
+	queries := r.URL.Query()
+	pageStr, sizeStr := queries.Get("page"), queries.Get("size")
+
+	// Catch existence
+	if len(pageStr) == 0 && len(sizeStr) != 0 || len(sizeStr) == 0 && len(sizeStr) != 0 {
+		return -1, -1, errors.New("bad request")
+	} else if len(pageStr) == 0 && len(sizeStr) == 0 {
+		return 0, 0, nil
+	}
+
+	page, err = strconv.Atoi(pageStr)
+	if err != nil {
+		return -1, -1, errors.New("invalid pagination: poor page value")
+	}
+
+	size, err = strconv.Atoi(sizeStr)
+	if err != nil {
+		return -1, -1, errors.New("invalid pagination: poor size value")
+	}
+
+	// Catch invalids
+	if size < 1 || page < 0 {
+		return -1, -1, errors.New("invalid pagination: too small value")
+	}
+
+	return page, size, nil
 }
