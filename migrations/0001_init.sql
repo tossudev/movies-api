4da925b1219224b00dd72df1dc003765ae1e0a11
Jere Meskanen <jere@macbook-2.local>
2026-08-26 11:20:14 +0300

    add: transaction to cover migrating instead of spamming!!

--- migrations/0001_init.sql
+++ migrations/0001_init.sql
@@ -1,26 +1,27 @@
-
 /*
 Genre (fields: id, name)
 Movie (fields: id, title, release_year, duration)
 Actor (fields: id, name, birth_date)
 */
 
+BEGIN TRANSACTION;
+
 CREATE TABLE IF NOT EXISTS genre (
-	id INTEGER PRIMARY KEY,
-	name TEXT							NOT NULL
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS movie (
-	id INTEGER PRIMARY KEY,
-	title TEXT							NOT NULL,
-	release_year INT					NOT NULL,
-	duration INT        NOT NULL
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    release_year INT NOT NULL,
+    duration INT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS actor (
-	id INTEGER PRIMARY KEY,
-	name TEXT							NOT NULL,
-	birth_date TEXT						NOT NULL
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    birth_date TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS movie_actors (
@@ -29,12 +30,14 @@ CREATE TABLE IF NOT EXISTS movie_actors (
     PRIMARY KEY (movie_id, actor_id),
     FOREIGN KEY (movie_id) REFERENCES movie(id) ON DELETE CASCADE,
     FOREIGN KEY (actor_id) REFERENCES actor(id) ON DELETE CASCADE
-  );
+);
 
-  CREATE TABLE IF NOT EXISTS movie_genres (
+CREATE TABLE IF NOT EXISTS movie_genres (
     movie_id INTEGER NOT NULL,
     genre_id INTEGER NOT NULL,
     PRIMARY KEY (movie_id, genre_id),
     FOREIGN KEY (movie_id) REFERENCES movie(id) ON DELETE CASCADE,
     FOREIGN KEY (genre_id) REFERENCES genre(id) ON DELETE CASCADE
-  );
+);
+
+COMMIT;
